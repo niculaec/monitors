4b01f8187dc6de85a9ae106b104fbde80da1df1c
niculaec <niculaec.alexandru@gmail.com>
2021-09-16 12:46:51 +0100

    Nested class Dimensions
    I create this nested class in order to use the parameters in our main class and I remeve unnecessary lines of code.

--- src/Monitor.java
+++ src/Monitor.java
@@ -2,20 +2,19 @@ import java.util.ArrayList;
 
 public class Monitor {
     public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(33,55,13);
         ArrayList<Monitor> monitor = new ArrayList<Monitor>();
         Monitor monitor1 = new Monitor("Lg","27","65","4k", Diagonal.diagonal24);
         Monitor monitor2 = new Monitor("Samsung", "32","80","fullHD",Diagonal.diagonal30);
 
     }
     String brand;
-    String dimension;
     String power;
     String resolution;
     public final Diagonal diagonal ;
 
     public Monitor(String brand, String dimension, String power, String resolution, Diagonal diagonal) {
         this.brand = brand;
-        this.dimension = dimension;
         this.power = power;
         this.resolution = resolution;
         this.diagonal = diagonal;
@@ -26,9 +25,6 @@ public class Monitor {
         return brand;
     }
 
-    public String getDimension() {
-        return dimension;
-    }
 
     public String getPower() {
         return power;
@@ -46,11 +42,23 @@ public class Monitor {
     public String toString() {
         return "Monitor{" +
                 "brand='" + brand + '\'' +
-                ", dimension='" + dimension + '\'' +
                 ", power='" + power + '\'' +
                 ", resolution='" + resolution + '\'' +
                 //", diagonal='" + diagonal + '\'' +
                 '}';
+    }
+
+    static class Dimensions {
+        int length;
+        int wide;
+        int height;
+
+        public Dimensions(int length, int wide, int height) {
+            this.length = length;
+            this.wide = wide;
+            this.height = height;
+        }
+
     }
 
     public enum Diagonal{
